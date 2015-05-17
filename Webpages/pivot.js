fd2650ec0a5396e2477e61acf06c9a3896ebb88f
jack9966qk <jack9966qk@MacMini.local>
2015-05-18 09:48:18 +1000

    Fixed typo when copy and past from IVLE test environment

--- Webpages/pivot.js
+++ Webpages/pivot.js
@@ -1,15 +1,4 @@
-$(function () {
-    var row = undefined, col = undefined, filter = undefined;
-
-    $(".draggable").draggable();
-
-    $("#row.droppable").droppable({
-        activate: function( event, ui ) {
-            $(this).addClass("dragging");
-        },
-        deactivate: function( event, ui ) {
-            $(this).removeClass("dragging");
-        },var row = undefined, col = undefined, filter = undefined;
+var row = undefined, col = undefined, filter = undefined;
 
 
 $(function () {
@@ -27,6 +16,7 @@ $(function () {
         drop: function( event, ui ) {
             $(this).addClass("dropped");
             row = ui.draggable.attr("id");
+            console.log(row);
         },
         out: function( event, ui ) {
             $(this).removeClass("dropped");
@@ -73,74 +63,6 @@ $(function () {
     $("button").click(function(){
         $("#generate").text("Loading...");
         console.log("row is " + row + " and col is " + col);
-        query = "pivot.py?row=" + row + "&col=" + col;
-        $.get(query, function(data, status){
-            $("#generate").text("Generate Another Table");
-        
-            var options = data
-            var colorAxis = {
-                min: 0,
-                minColor: '#FFFFFF',
-                maxColor: Highcharts.getOptions().colors[0]
-            };
-            options.colorAxis = colorAxis;
-            $('#container').highcharts(options);
-        });
-
-    });
-
-});
-        hoverclass: "hovered",
-        drop: function( event, ui ) {
-            $(this).addClass("dropped");
-            row = ui.draggable.attr("id");
-        },
-        out: function( event, ui ) {
-            $(this).removeClass("dropped");
-        }
-    });
-
-    $("#col.droppable").droppable({
-        activate: function( event, ui ) {
-            $(this).addClass("dragging");
-        },
-        deactivate: function( event, ui ) {
-            $(this).removeClass("dragging");
-        },
-        hoverclass: "hovered",
-        drop: function( event, ui ) {
-            $(this).addClass("dropped");
-            col = ui.draggable.attr("id");
-        },
-        out: function( event, ui ) {
-            $(this).removeClass("dropped");
-        }
-    });
-
-
-    $("#filter.droppable").droppable({
-        activate: function( event, ui ) {
-            $(this).addClass("dragging");
-        },
-        deactivate: function( event, ui ) {
-            $(this).removeClass("dragging");
-        },
-        hoverclass: "hovered",
-        drop: function( event, ui ) {
-            $(this).addClass("dropped");
-            filter = ui.draggable.attr("id");
-        },
-        out: function( event, ui ) {
-            $(this).removeClass("dropped");
-        }
-    });
-
-
-
-    $("button").click(function(){
-        $("#generate").text("Loading...");
-        row = $('input[name=row]:checked', '#tableForm').val();
-        col = $('input[name=col]:checked', '#tableForm').val();
         query = "pivot.py?row=" + row + "&col=" + col;
         $.get(query, function(data, status){
             $("#generate").text("Generate Another Table");
