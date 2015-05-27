64c1c180ef14b5f6ddcc4f5255ec1dfaa8a34fd5
jack9966qk <jack9966qk@MacMini.local>
2015-05-27 20:47:21 +1000

    Added comments to Python scripts

--- Webpages/map.py
+++ Webpages/map.py
@@ -4,9 +4,17 @@ import json
 from cgi import FieldStorage
 
 def get_airport_dict(csvFile):
+    """
+    csvFile - directory of the OpenFlight csvFile
+
+    returns a directory with keys as airport abbreviations, values as
+            a tuple of (latitude, longtitude)
+    """
+
     f = open(csvFile)
     rows = reader(f)
     d = {};
+    # iterate through the file and build the dictionary
     for row in rows:
         d[row[4].strip('"')] = [ float(row[6]), float(row[7]) ]
     return d
@@ -14,29 +22,42 @@ def get_airport_dict(csvFile):
 
 def generate_routes(csvFile, locDict, year, month, io = "Both"):
     """
+    csvFile - directory of the Australian Flights data
+    locDict - directory consists airport abbreviations and their locations
+              generated from get_airport_dict()
+    year, month - strings specifying the certain time period
+    io - specifying whether to read inbound flights and outbound flights
+         options are "I", "O", "Both"
+
     returns a dictionary
-     { (I/O, ((lat lon)...)): number }
+     {    ( "I" or "O", ((lat, lon)...) ) : total number of flights    }
     """
     
     f = open(csvFile)
     rows = DictReader(f)
     routeDict = defaultdict(int)
+
     for row in rows:
         if ("20" + row["Month"].split("-")[-1]) == year and row["Month"].split("-")[0] == month:
+            # in the required time range
             if io == "Both" or row["In/Out"] == io:
+                # IO type matches requirement
                 route = []
-                airports = row["Route"].split("-")
+                airports = row["Route"].split("-") # get all the airports in the route
                 for airport in airports:
                     if airport in locDict:
+                        # get coordinates for each of the airports
                         route.append( tuple(locDict[airport]) )
-
+                # add to the dictionary
                 routeDict[( row["In/Out"], tuple(route) )] += int(row["AllFlights"])    
     return routeDict.items()
+
 
 if __name__ == "__main__":
 
     form = FieldStorage()
 
+    # read all the parameters form query string
     year = form["year"].value
     month = form["month"].value
     io = form["IO"].value
--- Webpages/pivotHeader.py
+++ Webpages/pivotHeader.py
@@ -10,19 +10,19 @@ def print_filter_options(csvFile, header):
 
     prints a json array with all filter options for the given type
         [min, max] for numerical values
-        [A, B, C...] for string values
+        ["A", "B", "C"...] for string values
     """
 
     print "Content-Type: text/json"
     print  
     if header in ["AllFlights", "MaxSeats"]:
+        # numerical values
         print json.dumps(boundary_values(csvFile, header))
     else:
+        # strings
         print json.dumps(unique_values(csvFile, header))
 
 
 form = FieldStorage()
-
 header = form["header"].value
-
 print_filter_options("Data.csv", header)
